2e6dff1b61ad4665845db54690e5859e527317b1
paleman2019222 <61806840+paleman2019222@users.noreply.github.com>
2024-06-13 14:54:59 -0600

    unassign activity function

--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,8 @@ api.post('/saveActivity/:idU',mdAuth.ensureAuth,mdAuth.ensureAuthAdmin, activity
 api.get('/getAllActivities/:idU', mdAuth.ensureAuth, mdAuth.ensureAuthAdmin, activityController.getAllActivities);
 api.put('/assignActivity/:idA/:idAc/:idU',mdAuth.ensureAuth,mdAuth.ensureAuthAdmin,activityController.assignActivity);
 
-
+//pablo
+api.post('/unassignActivity/:idA/:idAc/:idU',mdAuth.ensureAuth,mdAuth.ensureAuthAdmin,activityController.unassignActivity);
 //JUan 
 api.get('/getUserActivities/:idU',mdAuth.ensureAuth,activityController.getUserActivities);
 
